9e5139b792dd09ef93744fa6eaa6c98a32dc7c7e
nikitabhutani <nikitabhutani024@gmail.com>
2023-07-30 03:58:02 +0530

    .

---
+++ empower_u/lib/widgets/auth_ui.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+class AuthUi extends StatelessWidget {
+  const AuthUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 220,
+          child: ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Colors.indigo.shade900)
+            ),
+            onPressed: (){}, child: Row(
+          children: [
+            Icon(Icons.phone_android_outlined,color:Colors.white),
+            SizedBox(width:8,),
+            Text('Continue With Phone',style: TextStyle(color: Colors.white),)
+          ],
+        ))),
+SignInButton(
+  Buttons.google,
+  text:('Continue With Google'), 
+  onPressed: (){},
+  ),
+  SignInButton(
+  Buttons.facebookNew,
+  text:('Continue With FaceBook'), 
+  onPressed: (){},
+  ),
+  Padding(
+    padding:const EdgeInsets.all(8.0),
+    child:Text('OR',style:TextStyle(color:Colors.indigo.shade900,fontWeight: FontWeight.bold,decoration: TextDecoration.underline)),),
+  Padding(
+    padding:const EdgeInsets.all(8.0),
+    child:Text('Login With Email',style:TextStyle(color:Colors.indigo.shade900,fontWeight: FontWeight.bold,decoration: TextDecoration.underline)),)
+      ],)
+    );
+  }
+}
