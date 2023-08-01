75b98b3334d0335304c56559eee29a58dff9368a
Namya Jain <jainnamya1306@gmail.com>
2023-08-01 10:41:39 +0530

    Updated asset path of logo

--- empower_u/lib/helper/contants.dart
+++ empower_u/lib/helper/contants.dart
@@ -1,6 +1,6 @@
 //This is the file for keeping all the constants like images and fonts
 import 'package:flutter/material.dart';
 
-Image logo = Image(image: AssetImage("assets/images/logo.png"));
+Image logo = Image(image: AssetImage("assets/images/logo.jpeg"));
 
 const TextStyle appBarStyle = TextStyle(color: Colors.black, fontSize: 16);
